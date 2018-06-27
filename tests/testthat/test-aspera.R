8a4efbf338b131bb8a582733d096f3f139ada05b
J. Kim <jkim2345@fredhutch.org>
2018-06-27 16:02:44 -0700

    Add unit tests for aspera

---
+++ tests/testthat/test-aspera.R
@@ -0,0 +1,19 @@
+context("aspera")
+
+test_that("`list_immport` works", {
+  res <- list_immport("/SDY1/StudyFiles")
+
+  expect_is(res, "list")
+  expect_equal(names(res), c("self", "items", "item_count", "total_count", "parameters"))
+})
+
+test_that("`download_immport` works", {
+  file_path <- "/SDY1/StudyFiles/Casale_Study_Summary_Report.doc"
+  temp_dir <- tempdir()
+
+  res <- download_immport(path = file_path, output_dir = temp_dir)
+
+  expect_is(res, "integer")
+  expect_equal(res, 0)
+  expect_true(basename(file_path) %in% dir(temp_dir))
+})
