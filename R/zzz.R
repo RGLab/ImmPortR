b0b31a1e0b6b9e1800e9dec73bd7787b3a64af6e
J. Kim <jkim2345@fredhutch.org>
2018-06-28 12:08:25 -0700

    Add package startup message to note user agreement

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("By retreiving data from ImmPort via ImmPortR, you confirm that you have read and accept all the terms and conditions without limitation of the User Agreement at http://www.immport.org/agreement")
+}
