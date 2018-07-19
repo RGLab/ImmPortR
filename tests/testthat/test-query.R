f6f09a6ebdb2b4b40e9a470a183bd2df7a34aaf9
Ju Yeong Kim <jkim2345@fredhutch.org>
2018-07-19 11:02:18 -0700

    Add test for query_filePath

--- tests/testthat/test-query.R
+++ tests/testthat/test-query.R
@@ -1,10 +1,17 @@
 context("query")
 
 test_that("`query_dataset` works", {
-  res <- query_dataset(dataset = "elisa", study = "SDY269")
+  res <- query_dataset("elisa", "SDY269")
 
   expect_is(res, "data.frame")
   expect_length(res, 41)
 
   expect_error(query_dataset("invalidDataset", "SDY269"))
+})
+
+test_that("`query_filePath` works", {
+  res <- query_filePath("SDY269")
+
+  expect_is(res, "data.frame")
+  expect_length(res, 37)
 })
