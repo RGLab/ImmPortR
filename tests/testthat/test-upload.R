1b077edb256e2e080a53f98651816be521d47566
J. Kim <jkim2345@fredhutch.org>
2022-09-01 13:41:20 -0700

    Enable upload unit tests

--- tests/testthat/test-upload.R
+++ tests/testthat/test-upload.R
@@ -1,11 +1,11 @@
 context("upload")
 
-server <- "immport-upload.niaid.nih.gov:8443"
+is_server_up <- pingr::is_up("immport-upload.niaid.nih.gov", 8443L)
 upload_ticket <- "jkim2345_20171213_15861"
 validation_ticket <- "jkim2345_20171213_15860"
 
 test_that("`generate_templates` works", {
-  skip_if_offline(server)
+  skip_if_not(is_server_up)
 
   output_file <- generate_templates(5851, output_dir = tempdir())
 
@@ -17,20 +17,22 @@ test_that("`generate_templates` works", {
 })
 
 test_that("`list_workspaces` works", {
-  skip_if_offline(server)
+  skip_if_not(is_server_up)
 
   res <- list_workspaces()
 
   expect_is(res, "list")
-  expect_equal(names(res[[1]]),
-               c("workspaceId", "name", "type",
-                 "category", "dateCreated",
-                 "dateLastUpdated", "createdBy",
-                 "lastUpdatedBy"))
+  expect_equal(
+    object = names(res[[1]]),
+    expected = c(
+      "workspaceId", "name", "type", "category", "dateCreated",
+      "dateLastUpdated", "createdBy", "lastUpdatedBy"
+    )
+  )
 })
 
 test_that("`check_status` works", {
-  skip_if_offline(server)
+  skip_if_not(is_server_up)
 
   res <- check_status(upload_ticket)
 
@@ -40,21 +42,25 @@ test_that("`check_status` works", {
 })
 
 test_that("`get_ticket_summary` works", {
-  skip_if_offline(server)
+  skip_if_not(is_server_up)
 
   res <- get_ticket_summary(upload_ticket)
 
   expect_is(res, "list")
-  expect_equal(names(res), c("uploadRegistrationId", "workspaceName",
-                             "fileName", "status", "uploadTicketNumber",
-                             "dateCreated", "createdBy", "uploadMethod",
-                             "uploadRegistrationResults"))
+  expect_equal(
+    object = names(res),
+    expected = c(
+      "uploadRegistrationId", "workspaceName", "fileName", "status",
+      "uploadTicketNumber", "dateCreated", "createdBy", "uploadMethod",
+      "uploadRegistrationResults"
+    )
+  )
 
   expect_error(get_ticket_summary("invalid_19991231_99999"))
 })
 
 test_that("`download_ticket_report` works", {
-  skip_if_offline(server)
+  skip_if_not(is_server_up)
 
   res <- download_ticket_report(upload_ticket, output_dir = tempdir())
 
